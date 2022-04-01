9b50559be35ba44e4bbffe476f81e5b5e5c4bc40
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2022-03-31 18:46:39 -0700

    Testing loadout card view

--- components/LoadoutCard.js
+++ components/LoadoutCard.js
@@ -9,9 +9,23 @@ export default function LoadoutCard({ weapon }) {
       <div className="loadout-card-body">
         <div className="loadout-card-col-1">
           <h3>{`name`}</h3>
+          <div>
+            <Image
+              src="/images/primaries/STG44.png"
+              width={1175}
+              height={490}
+            />
+          </div>
         </div>
         <div className="loadout-card-col-2">
-          <p>Testing</p>
+          <h3>{`name`}</h3>
+          <div>
+            <Image
+              src="/images/primaries/STG44.png"
+              width={1175}
+              height={490}
+            />
+          </div>
         </div>
       </div>
     </div>
